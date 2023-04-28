be1016e7112de5eeb8e3389c5d85f296c215f636
Interlandi Rosario <Interlandi Rosario@172.22.17.96>
2023-04-28 12:41:45 +0200

    Implementazione dell'algoritmo e dell'interfaccia.

--- src/main/java/it/polito/tdp/Lab01/FXMLController.java
+++ src/main/java/it/polito/tdp/Lab01/FXMLController.java
@@ -19,6 +19,9 @@ public class FXMLController {
 
     @FXML
     private URL location;
+    
+    @FXML
+    private Button btnCancel;
 
     @FXML
     private Button btnInserisci;
@@ -31,24 +34,55 @@ public class FXMLController {
 
     @FXML
     private TextArea txtResult;
+    
+    @FXML
+    private TextArea txtTime;
+    
+    @FXML
+    void handleCancel(ActionEvent event) {
+    	long t1=System.nanoTime();
+    	String parola = txtParola.getText();
+    	elenco.doCancel(parola);
+    	long t2 = System.nanoTime();
+    	txtTime.appendText((t2-t1)+"\n");
+    }
 
     @FXML
     void doInsert(ActionEvent event) {
-
+    	long t1=System.nanoTime();
+    	String parola = txtParola.getText();
+    	if (parola.compareTo("")==0) {
+    		String elencoInStringa="";
+    		for(String p:elenco.getElenco()) {
+    			if (elencoInStringa.compareTo("")!=0)
+    				elencoInStringa += "\n";
+    			elencoInStringa += p;
+    		}
+    		txtResult.setText(elencoInStringa);	
+    	}
+    	else {
+    		elenco.addParola(parola);
+    	}
+    	long t2 = System.nanoTime();
+    	txtTime.appendText((t2-t1)+"\n");
     }
 
     @FXML
     void doReset(ActionEvent event) {
-
+    	long t1=System.nanoTime();
+    	elenco.reset();
+    	long t2 = System.nanoTime();
+    	txtTime.appendText((t2-t1)+"\n");
     }
 
     @FXML
     void initialize() {
+    	assert btnCancel != null : "fx:id=\"btnCancel\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnInserisci != null : "fx:id=\"btnInserisci\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtParola != null : "fx:id=\"txtParola\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-        
+        assert txtTime != null : "fx:id=\"txtTime\" was not injected: check your FXML file 'Scene.fxml'.";
         elenco = new Parole() ;
     }
 
--- src/main/java/it/polito/tdp/Lab01/model/Parole.java
+++ src/main/java/it/polito/tdp/Lab01/model/Parole.java
@@ -1,24 +1,36 @@
 package it.polito.tdp.Lab01.model;
 
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Parole {
-		
+	LinkedList<String> listaParole;
 	public Parole() {
-		//TODO
+		listaParole = new LinkedList<String>();
 	}
 	
 	public void addParola(String p) {
-		//TODO
+		listaParole.add(p);
 	}
-	
+	/**
+	 * I have to create a function that return a list of string
+	 * in alphabetical order
+	 * @return Collection<String>
+	 */
 	public List<String> getElenco() {
-		//TODO
-		return null;
+		LinkedList<String> elenco = new LinkedList<String>(listaParole);
+		Collections.sort(elenco, new comparatoreStringhe());
+		return elenco;
 	}
 	
 	public void reset() {
-		// TODO
+		listaParole.clear();
+	}
+	public void doCancel(String s) {
+		if (listaParole.contains(s)) {
+			listaParole.remove(s);
+		}
 	}
 
 }
